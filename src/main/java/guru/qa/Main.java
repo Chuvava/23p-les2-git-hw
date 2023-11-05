d47361837a734d10d7174ad08fd20fff564222db
Chuvava <aleksandr.efimov94@mail.ru>
2023-11-05 18:08:44 +0100

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,8 +1,12 @@
 package guru.qa;
 
 public class Main {
+
+    String msg = "random message here";
+    
     public static void main(String[] args) {
 
+        System.out.println(msg);
         System.out.println("Hello qa.guru!");
     }
 }
