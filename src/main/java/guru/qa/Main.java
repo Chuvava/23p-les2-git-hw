6df4135fe9a5e823e131497823bf0c01c245f9cb
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2023-11-05 18:09:47 +0100

    added getMeassge method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,6 +3,11 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello qa.guru!");
+        System.out.println("Hello qa.guru!!!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "new message here";
     }
 }
